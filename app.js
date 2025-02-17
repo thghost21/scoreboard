382fce90e99295b42ef7a6e694f9d06fd75bb44a
thghost21 <tylerhacker21@gmail.com>
2025-02-17 16:21:11 -0700

    updated documentation, added 3 pointers

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ console.log("ready")
 let home = 0
 let away = 0
 
+
+//scoring buttons and checks
 function home1point() {
   console.log('Home score plus one')
   home += 1
@@ -14,10 +16,24 @@ function home1point() {
   UpdateHomeScore()
   CheckWinner()
 }
+function home3point() {
+  console.log('Home score plus 3')
+  home += 3
+  console.log(home)
+  UpdateHomeScore()
+  CheckWinner()
+}
 
-function Away1point() {
+function away1point() {
   console.log('Away score plus one')
   away += 1
+  console.log(away)
+  UpdateAwayScore()
+  CheckWinner()
+}
+function away3point() {
+  console.log('away score plus 3')
+  away += 3
   console.log(away)
   UpdateAwayScore()
   CheckWinner()
@@ -33,16 +49,25 @@ function UpdateAwayScore() {
   awayScoreElem.innerText = away
 
 }
-
+//checks the winner as long as score is >= 10, then sends alert saying who has won and returns the score to 0 to start again
 function CheckWinner() {
-  if (home == 10) {
+
+  if (home >= 10) {
     console.log("Home Wins")
     window.alert("Home has Won!")
+    home = 0
+    away = 0
+    UpdateAwayScore()
+    UpdateHomeScore()
 
   }
-  else if (away == 10) {
+  else if (away >= 10) {
     console.log("Away Wins")
     window.alert("Away has Won!")
+    home = 0
+    away = 0
+    UpdateAwayScore()
+    UpdateHomeScore()
 
   }
   else {
